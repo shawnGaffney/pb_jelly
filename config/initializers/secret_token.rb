344f837bf073b788bb789bb734389d61e06309f9
Shawn <gaffneyshawn23@gmail.com>
2011-10-13 16:19:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PbJelly::Application.config.secret_token = '13c999b8f85c51d2459a3bc284f3fdb8452d2f87823c000ef5ee99e5b51bb7812b13c0121b3c87701c0c681b0203845930843b496a9839931e46e770934f355b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PbJelly::Application.config.session_store :cookie_store, :key => '_pb_jelly_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PbJelly::Application.config.session_store :active_record_store
